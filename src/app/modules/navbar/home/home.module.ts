7ef86c7c7742641db9955fd4f504ec038ef1d2df
Yeni Hernandez <yeni.hernandez@udea.edu.co>
2024-04-22 19:38:05 -0500

    Fix General Styles

--- src/app/modules/navbar/home/home.module.ts
+++ src/app/modules/navbar/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -13,7 +13,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SharedModule
+    SharedModule,
+    MatGridListModule,
   ]
 })
 export class HomeModule { }
