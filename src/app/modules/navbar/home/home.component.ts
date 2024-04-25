14ca73f332cbb1d2758b7292d8be68aeb6949222
Yeni Hernandez <yeni.hernandez@udea.edu.co>
2024-04-25 18:49:46 -0500

    Fix Styles Flex-grid

--- src/app/modules/navbar/home/home.component.ts
+++ src/app/modules/navbar/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 export interface Tile {
-  color: string;
+  color?: string;
+  imageURL? : string;
   cols: number;
   rows: number;
   text: string;
@@ -12,9 +13,13 @@ export interface Tile {
 })
 export class HomeComponent {
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'One', cols: 2, rows: 3, imageURL: '../../../../assets/banner.jpg'},
+    {text: 'Two', cols: 1, rows: 2, imageURL: '../../../../assets/banner.jpg'},
+    {text: 'Three', cols: 2, rows: 2,imageURL: '../../../../assets/banner.jpg' },
+    {text: 'Four', cols: 1, rows: 2,imageURL: '../../../../assets/banner.jpg' },
+    {text: 'Five', cols: 1, rows: 3, imageURL: '../../../../assets/banner.jpg'},
+    {text: 'Six', cols: 3, rows: 3, imageURL: '../../../../assets/banner.jpg'},
+    {text: 'Seven', cols: 2, rows: 2,imageURL: '../../../../assets/banner.jpg' },
+    
   ];
 }
