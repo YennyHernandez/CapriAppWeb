17bddaff33e35f6664082d50a7513c03ef8a4306
Yeni Hernandez <yeni.hernandez@udea.edu.co>
2024-10-15 14:24:05 -0500

    fix calendar date disabled

--- src/app/shared/google-calendar/google-calendar.component.ts
+++ src/app/shared/google-calendar/google-calendar.component.ts
@@ -1,46 +1,49 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleEventService } from 'src/app/services/google-events.service';
+
 @Component({
   selector: 'app-google-calendar',
   templateUrl: './google-calendar.component.html',
   styleUrls: ['./google-calendar.component.scss']
 })
 export class GoogleCalendarComponent implements OnInit {
-  //@Input() formControlName!: string;  // formControl desde el componente padre
   reservedDates: Date[] = [];
   selectedDate: Date | null = null;
 
-  constructor(private googleEventService: GoogleEventService) { }
+  constructor(private googleEventService: GoogleEventService) {}
 
   ngOnInit(): void {
     this.loadReservedDates();
-
   }
 
   loadReservedDates() {   
-      this.googleEventService.loadGapi().then(() => {
-        console.log("entrando al loadreserves")
-        this.googleEventService.listGoogleEvents().then((events: any[]) => {
-          console.log("haciendo peticion a listar")
-          this.reservedDates = events.map(event => new Date(event.start.dateTime));
-          console.log(this.reservedDates, " fechas reservadass")
-        });
-      })
-
+    this.googleEventService.loadGapi().then(() => {
+      this.googleEventService.listGoogleEvents().then((events: any[]) => {
+        this.reservedDates = events.map(event => new Date(event.start.dateTime));
+        console.log(this.reservedDates, " fechas reservadas");
+      });
+    });
   }
 
   disableReservedDates = (date: Date | null): boolean => {
-    if (!date) return true;
-    return !this.reservedDates.some(d => d.getTime() === date.getTime());
+    if (!date) return true; // Si no hay fecha seleccionada, permite seleccionar
+    // Verifica si la fecha está en la lista de fechas reservadas
+    console.log(date, '🌱🌱🌱')
+    return this.reservedDates.some(d => 
+      d.getFullYear() === date.getFullYear() && 
+      d.getMonth() === date.getMonth() && 
+      d.getDate() === date.getDate()
+    )? false: true ;
   }
+
   onDateChange(event: any): void {
-    const selected = new Date(event.target.value);
+    const selected = new Date(event.value);
     if (this.disableReservedDates(selected)) {
       console.warn("Esta fecha está reservada");
+      this.selectedDate = null; // O cualquier lógica que necesites para manejar fechas reservadas
     } else {
       this.selectedDate = selected;
       console.log("Fecha seleccionada:", this.selectedDate);
     }
   }
 }
-
